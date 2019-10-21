b5533e8f48021d0471b4e8a564903fcca3a071f3
prasantib <prasanti.babu@gmail.com>
2019-10-21 16:16:03 -0400

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String [] args){
+        //Printing out hello world to the console
         System.out.println("Hello World");
+        //declaration of a variable
+        String name;
+        //initialization of a variable
+        name = "Prasanti";
+        //declaration and initialization of a variable in the same line
+        String newName = "Brad";
+        //reinitializing a variable
+        name = "Sam";
+        System.out.println(name);
     }
 }
